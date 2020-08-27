762e8b1ec3277532c0feb179c68f2ee1207e5633
carlo briganti <505641nl@saeinstitute.edu>
2020-08-27 18:04:57 +0200

    funzione per pulire la tavola

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 $(document).ready(function () {
-    // popolo dinamicamnete il container nel dom di quadrati colorati
-    for(var i=0;i<36;i++){
-        $('.container').append('<div class="quadrati"></div>');
-    }
-  $(".quadrati").one('click',function () {
-      var square = $(this);
-      $.ajax(
-          {
-      'url': "https://flynn.boolean.careers/exercises/api/random/int",
-      'method': "GET",
-      'success': function (risposta) {
-          var risposta = risposta.response;
-          console.log(risposta);
-          if (risposta <= 5){
-              square.addClass('green');
-              square.append(risposta);
-            }else{
-            square.addClass('yellow');
-            square.append(risposta);
-            };
-        },
+  // popolo dinamicamnete il container nel dom di quadrati colorati
+  for (var i = 0; i < 36; i++) {
+    $(".container").append('<div class="quadrati"></div>');
+  }
+  $(".quadrati").one("click", function () {
+    var square = $(this);
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function (risposta) {
+        var risposta = risposta.response;
+        console.log(risposta);
+        if (risposta <= 5) {
+          square.addClass("green");
+          square.append(risposta);
+        } else {
+          square.addClass("yellow");
+          square.append(risposta);
+        }
+      },
       error: function () {
         alert("si e verificato un errore");
-      }
+      },
     });
   });
+  $('.button').click(function(){
+     init();
+  });
+  function init() {
+    var quadrati = $(".quadrati");
+    quadrati.removeClass("yellow");
+    quadrati.removeClass("green");
+    quadrati.text('');
+  };
 });
